1dce6a9fee412a64886f02f3e136b3c9976c3239
Ollebaack <ollebaack@gmail.com>
2017-01-23 14:25:37 +0100

    Competition update.

--- simhopp/simhopp/Competition.cs
+++ simhopp/simhopp/Competition.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+
 
 namespace simhopp
 {
   
     public class Competition
     {
-
+        private string name;
+        private List<string> CompetitorsList;
     }
 }
