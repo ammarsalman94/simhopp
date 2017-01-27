73306b2f6b8c026d9fdca017d967da1c6510f777
Joel <joelfolkessonn@gmail.com>
2017-01-27 10:56:14 +0100

    borttagning av testfunktion.

--- simhopp/simhopp/JumpType.cs
+++ simhopp/simhopp/JumpType.cs
@@ -12,22 +12,6 @@ namespace simhopp
         private SortedDictionary<string, int> jumpStats; // jumpstats[0] == (volt, 10m)
         private SortedDictionary<int,string> startPos; // startPos[0] == (1, forward)
                                                        // jumpstart[0] && startPos[0] &&
-
-
-        List<List<string>> myList = new List<List<string>>();
-        IList.Add(new List<string> { "a", "b" });
-        myList.Add(new List<string> { "c", "d", "e" });
-        myList.Add(new List<string> { "qwerty", "asdf", "zxcv" });
-        myList.Add(new List<string> { "a", "b" });
-
-        // To iterate through it.
-        foreach(List<string> subList in myList)
-        {
-            foreach (string item in subList)
-            {
-                Console.WriteLine(item);
-            }
-        }
-    }
+     }
 }
 
