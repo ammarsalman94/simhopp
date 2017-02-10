b1ba13cb26dab55c9adb564ccd12a0f46eccda16
Ollebaack <ollebaack@gmail.com>
2017-02-10 13:34:29 +0100

    diveNumberTranslator()

--- simhopp/simhopp/Judge.cs
+++ simhopp/simhopp/Judge.cs
@@ -41,10 +41,123 @@ namespace simhopp
 
         }
 
-        public void sendScoreToCompetitor(double point)
+        //public int countJudges(Competition game)
+        //{
+
+        //    int amountJudges;
+
+        //    amountJudges = game.countJudges();
+
+        //    return amountJudges;
+        //}
+
+        public  void UseParams(params int[] list)
         {
-            var points = new Points();
-            points.SendPointToCompetitor(point);
+            for (int i = 0; i < list.Length; i++)
+            {
+                Console.Write(list[i] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public double sendScoreToCompetitor(Competition game, Competitor player)          //In progress
+        {
+            //var points = new Points();
+            //points.SendPointToCompetitor(point);
+            double Fail = 0;
+            int amountJudges;
+            amountJudges = game.countJudges();
+
+            double Judge1;
+            double Judge2;
+            double Judge3;
+            double Judge4;
+            double Judge5;
+            double Judge6;
+            double Judge7;
+            double Judge8;
+            double Judge9;
+            double Judge10;
+
+            switch (amountJudges)
+            {
+            case 1:
+                case 2:
+                    return Fail;
+            case 3:
+
+                    //sin poäng
+                    //Judge1 = 2;
+                    //Judge2 = 3.6;
+                    //Judge3 = 5;
+                    return score;
+
+                    
+            case 4:
+                    Judge1 = 2;
+                    Judge2 = 3.6;
+                    Judge3 = 5;
+                    Judge4 = 2;
+                    return score;
+            case 5:
+                    Judge1 = 2;
+                    Judge2 = 3.6;
+                    Judge3 = 5;
+                    Judge4 = 2;
+                    Judge5 = 1;
+                    return score;
+            case 6:
+                    Judge1 = 2;
+                    Judge2 = 3.6;
+                    Judge3 = 5;
+                    Judge4 = 2;
+                    Judge5 = 1;
+                    Judge6 = 0;
+                    return score;
+            case 7:
+                    Judge1 = 2;
+                    Judge2 = 3.6;
+                    Judge3 = 5;
+                    Judge4 = 2;
+                    Judge5 = 1;
+                    Judge6 = 0;
+                    Judge7 = 7.3;
+                    return score;
+            case 8:
+                    Judge1 = 2;
+                    Judge2 = 3.6;
+                    Judge3 = 5;
+                    Judge4 = 2;
+                    Judge5 = 1;
+                    Judge6 = 0;
+                    Judge7 = 7.3;
+                    Judge8 = 4.5;
+                    return score;
+            case 9:
+                    Judge1 = 2;
+                    Judge2 = 3.6;
+                    Judge3 = 5;
+                    Judge4 = 2;
+                    Judge5 = 1;
+                    Judge6 = 0;
+                    Judge7 = 7.3;
+                    Judge8 = 4.5;
+                    Judge9 = 2.5;
+                    return score;
+            case 10:
+                    Judge1 = 2;
+                    Judge2 = 3.6;
+                    Judge3 = 5;
+                    Judge4 = 2;
+                    Judge5 = 1;
+                    Judge6 = 0;
+                    Judge7 = 7.3;
+                    Judge8 = 4.5;
+                    Judge9 = 2.5;
+                    Judge10 = 3.4;
+                    return score;
+            }
+            return Fail;
         }
 
         public double setPoint()
